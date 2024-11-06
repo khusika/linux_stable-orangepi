6f772e5becbda042d6896730475f5ad5146f3c33
Zefa Chen <zefa.chen@rock-chips.com>
2024-11-06 17:13:20 +0800

    media: rockchip: vicap skip wait for stop, when work in online mode and toisp is off
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>
    Change-Id: I30e822b6aea7d455765ae798f86322d4ee395d92

--- drivers/media/platform/rockchip/cif/capture.c
+++ drivers/media/platform/rockchip/cif/capture.c
@@ -6324,8 +6324,10 @@ void rkcif_do_stop_stream(struct rkcif_stream *stream,
 			} else {
 				stream->stopping = true;
 			}
-		} else {
+		} else if (dev->sditf[0] && (!dev->sditf[0]->is_toisp_off)) {
 			stream->stopping = true;
+		} else {
+			rkcif_stream_stop(stream);
 		}
 		if (stream->stopping == true) {
 			if (mode == RKCIF_STREAM_MODE_TOISP && dev->sditf[0]->is_toisp_off)
